ab7e80ab589fb6b0f3db05a97d4e4df4165dffac
Claudia Beresford <cberesford@pivotal.io>
2018-12-07 13:03:47 +0000

    part 6

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,23 +1,9 @@
-function isDivisibleByThree(number) {
-  if (number % 3 === 0) {
-    return true;
-  }
-}
-
-function isDivisibleByFive(number) {
-  if (number % 5 === 0) {
-    return true;
-  }
-}
-
-function isDivisibleByThreeAndFive(number) {
-  if (number % 15 === 0) {
+function isDivisibleBy(number, divisor) {
+  if (number % divisor === 0) {
     return true;
   }
 }
 
 module.exports = {
-  isDivisibleByThree: isDivisibleByThree,
-  isDivisibleByFive: isDivisibleByFive,
-  isDivisibleByThreeAndFive: isDivisibleByThreeAndFive
+  isDivisibleBy: isDivisibleBy
 }
--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -2,26 +2,26 @@ const fizzbuzz = require('./fizzbuzz');
 
 describe('Fizzbuzz', () => {
   test('knows when a number is divisible by 3',() => {
-    expect(fizzbuzz.isDivisibleByThree(3)).toBeTruthy();
+    expect(fizzbuzz.isDivisibleBy(3, 3)).toBeTruthy();
   });
 
   test('knows when a number NOT is divisible by 3',() => {
-    expect(fizzbuzz.isDivisibleByThree(1)).toBeFalsy();
+    expect(fizzbuzz.isDivisibleBy(1, 3)).toBeFalsy();
   });
 
   test('knows when a number is divisible by 5',() => {
-    expect(fizzbuzz.isDivisibleByFive(5)).toBeTruthy();
+    expect(fizzbuzz.isDivisibleBy(5, 5)).toBeTruthy();
   });
 
   test('knows when a number is NOT divisible by 5',() => {
-    expect(fizzbuzz.isDivisibleByFive(1)).toBeFalsy();
+    expect(fizzbuzz.isDivisibleBy(1, 5)).toBeFalsy();
   });
 
   test('knows when a number is divisible by 3 and 5',() => {
-    expect(fizzbuzz.isDivisibleByThreeAndFive(15)).toBeTruthy();
+    expect(fizzbuzz.isDivisibleBy(15, 15)).toBeTruthy();
   });
 
   test('knows when a number is NOT divisible by 3 and 5',() => {
-    expect(fizzbuzz.isDivisibleByThreeAndFive(1)).toBeFalsy();
+    expect(fizzbuzz.isDivisibleBy(1, 15)).toBeFalsy();
   });
 });
